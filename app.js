e1abd7b18e3cc151197d33d93e563f7a8211163c
David Eliason <david@davethemaker.com>
2018-07-05 20:09:13 -0700

    use of modules.exports

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
 var user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello there ${user.username}!`, (err) => {
+fs.appendFile('greetings.txt', `Hello there ${user.username}! Color is ${notes.color}`, (err) => {
     if (err) console.error(err);
 });
